f5dc96824687326fb1d85f449ca2838e528cacc9
SANJANAEPARI <89684461+SANJANAEPARI@users.noreply.github.com>
2021-10-17 12:18:58 +0530

    Create takeinput.cpp

---
+++ Milestone1/takeinput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int num;
+   cout<<"Enter the number:\n";
+   cin>>num;
+   cout<<"The number entered by user is "<<num;
+   return 0;
+   }
